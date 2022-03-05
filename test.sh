a6f5dfae119662f8780e66d070e12bd267f88381
Jie Li / jli675 <61730145+366401555@users.noreply.github.com>
2022-03-04 16:15:24 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo "Creating sequence..."
 cat <(echo 'CREATE SEQUENCE part_number_seq  START WITH 50000;')|psql -h localhost -p $PGPORT $USER"_DB"
 
 echo "Creating trigger and procedure..."
-psql -h localhost -p $PGPORT $DB_NAME < triggers.sql
+psql -h localhost -p $PGPORT $USER"_DB" < triggers.sql
 
 echo "Testing insert..."
 cat <(echo "Insert into part_nyc(supplier, color, on_hand, descr) Values (0, 0, 20, 'Description')")|psql -h localhost -p $PGPORT $USER"_DB"
