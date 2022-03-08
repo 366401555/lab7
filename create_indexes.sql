ede42b520f31d0859ad10097cd30a32ac2f515e8
Jie Li / jli675 <61730145+366401555@users.noreply.github.com>
2022-03-08 13:16:55 -0800

    Update create_indexes.sql

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,3 +1,19 @@
+#part_ny
 DROP INDEX IF EXISTS nyc_part_on_hand_idx;
 CREATE INDEX nyc_part_on_hand_idx
 ON part_nyc(on_hand);
+
+DROP INDEX IF EXISTS nyc_part_supplier_idx;
+CREATE INDEX nyc_part_supplier_idx
+ON part_nyc(supplier);
+
+
+
+#part_sfo
+DROP INDEX IF EXISTS sfo_part_on_hand_idx;
+CREATE INDEX sfo_part_on_hand_idx
+ON #part_sfo(on_hand);
+
+DROP INDEX IF EXISTS sfo_part_supplier_idx;
+CREATE INDEX sfo_part_supplier_idx
+ON part_sfo(supplier);
