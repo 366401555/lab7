9c57d840f4a0eb4c8ff66e999395c6c8434d0a06
Jie Li / jli675 <61730145+366401555@users.noreply.github.com>
2022-03-08 18:09:42 -0800

    Update create_indexes.sql

--- create_indexes.sql
+++ create_indexes.sql
@@ -2,18 +2,31 @@
 DROP INDEX IF EXISTS part_nyc_on_hand_idx;
 CREATE INDEX part_nyc_on_hand_idx
 ON part_nyc USING BTREE (on_hand);
-
-DROP INDEX IF EXISTS part_nyc_supplier_idx;
-CREATE INDEX part_nyc_supplier_idx
-ON part_nyc USING BTREE (supplier);
-
-
-
-
 DROP INDEX IF EXISTS part_sfo_on_hand_idx;
 CREATE INDEX part_sfo_on_hand_idx
 ON part_sfo USING BTREE (on_hand);
 
+
+DROP INDEX IF EXISTS part_nyc_supplier_idx;
+CREATE INDEX part_nyc_supplier_idx
+ON part_nyc USING BTREE (supplier);
 DROP INDEX IF EXISTS part_sfo_supplier_idx;
 CREATE INDEX part_sfo_supplier_idx
 ON part_sfo USING BTREE (supplier);
+
+DROP INDEX IF EXISTS part_nyc_part_number_idx;
+CREATE INDEX part_nyc_part_number_idx 
+ON part_nyc USING BTREE (part_number);
+DROP INDEX IF EXISTS part_sfo_part_number_idx;
+CREATE INDEX part_sfo_part_number_idx 
+ON part_sfo USING BTREE (part_number);
+
+DROP INDEX IF EXISTS part_nyc_color_idx;
+CREATE INDEX part_nyc_color_idx 
+ON part_nyc USING BTREE (color);
+DROP INDEX IF EXISTS part_sfo_color_idx;
+CREATE INDEX part_sfo_color_idx 
+ON part_sfo USING BTREE (color);
+
+
+
