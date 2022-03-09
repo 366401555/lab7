a6f888f1c6b238ef3066d4b4a50912344389bb8d
Jie Li / jli675 <61730145+366401555@users.noreply.github.com>
2022-03-08 18:01:22 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,17 +5,22 @@ SELECT COUNT(part_number)
 SELECT SUM((SELECT COUNT(part_nyc.on_hand) 
 	 FROM part_nyc, color
 	 WHERE part_nyc.color = color.color_id AND color.color_name = 'Red') 
-	 UNION ALL 
+	 + 
 	 (SELECT COUNT(part_sfo.on_hand)
 	FROM part_sfo, color
 	WHERE part_sfo.color = color.color_id AND color.color_name = 'Red'));
 			 
-SELECT supplier.supplier_name
-	FROM supplier
-	INNER JOIN part_nyc ON supplier.supplier_id = part_nyc.supplier
-	INNER JOIN part_sfo ON supplier.supplier_id = part_sfo.supplier
-	GROUP BY supplier.supplier_name
-	HAVING SUM(part_nyc.on_hand) > SUM(part_sfo.on_hand);
+SELECT supplier.supplier_name 
+FROM supplier
+WHERE
+(SELECT SUM(part_nyc.on_hand)
+FROM part_nyc
+WHERE part_nyc.supplier = supplier.supplier_id)
+>
+(SELECT SUM(part_sfo.on_hand)
+FROM part_sfo
+WHERE part_sfo.supplier = supplier.supplier_id)
+GROUP BY supplier.supplier_name;
 	
 	
 SELECT supplier.supplier_name 
